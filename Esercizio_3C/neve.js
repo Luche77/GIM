06b698798b2c658584eb2b0c7c2155af6d96580c
lucadellagostino <lucadellagostino77@gmail.com>
2023-05-15 22:06:29 +0200

    Esercizi Aggiornati

--- Esercizio_3C/neve.js
+++ Esercizio_3C/neve.js
@@ -2,6 +2,7 @@ function createSnowflake() {
 	const snowflake = document.createElement('div');
 	snowflake.classList.add('snowflake');
 	snowflake.style.left = Math.random() * window.innerWidth + 'px';
+	snowflake.style.bottom = window.innerHeight + 20 + 'px';
 	snowflake.style.animationDelay = Math.random() * 5 + 's';
 	document.body.appendChild(snowflake);
 	setTimeout(() => {
